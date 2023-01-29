22e75cc1957311593451897e7a4300fe0d7b1611
Xeloreon <94229507+iqrobanyuanto@users.noreply.github.com>
2023-01-29 22:00:28 +0700

    update note
    note requirement

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Enter a list of numbers separated by a comma: ");
+        String input = "23,2,123,1";
+        String[] numbers = input.split(",");
+        for (String number : numbers) {
+          int num = Integer.parseInt(number);
+          System.out.println(num);
+        }
+    }
+
+    public static void definenum(String[] numbers){
+
     }
 }
