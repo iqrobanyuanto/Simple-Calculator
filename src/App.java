8ebb426161df67cb645c2b50551ed95e7e4ced4d
Xeloreon <94229507+iqrobanyuanto@users.noreply.github.com>
2023-01-30 16:30:06 +0700

    update app

--- src/App.java
+++ src/App.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-      Scanner input = new Scanner(System.in);
-      Integer[] pack = new Integer[2];
-      //input
-      System.out.print("Input number: ");
-      Integer a = input.nextInt();
-      System.out.print("Input Operator: ");
-      char x = input.next().charAt(0);
-      System.out.print("Input number: ");
-      Integer b = input.nextInt();
-      input.close();
-      //pack
-      packNUM(pack,a,b);
-      //prInteger
-      KalkulasiPerhitungan.defOP(pack, x);;
+        Scanner input = new Scanner(System.in);
+        Integer[] pack = new Integer[2];
+        //input
+        System.out.print("Input number: ");
+        Integer a = input.nextInt();
+        System.out.print("Input Operator: ");
+        char x = input.next().charAt(0);
+        System.out.print("Input number: ");
+        Integer b = input.nextInt();
+        input.close();
+        //pack
+        packNUM(pack,a,b);
+        //prInteger
+        KalkulasiPerhitungan.defOP(pack, x);;
     }
-//
     public static void packNUM(Integer[] packs,Integer a, Integer b){
       packs[0] = a;
       packs[1] = b;
