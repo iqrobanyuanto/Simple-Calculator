cc90c3c6403c58ff363fcb5d6d5170e0a86d8a7d
Xeloreon <94229507+iqrobanyuanto@users.noreply.github.com>
2023-01-30 16:13:13 +0700

    update #3
    penambahan batasan kalkulasi

--- src/KalkulasiPerhitungan.java
+++ src/KalkulasiPerhitungan.java
@@ -1,6 +1,7 @@
 public class KalkulasiPerhitungan {
     public static void defOP(Integer number[], char operator){
         int r = 0;
+        //
         switch(operator){
             case '+':
             r = number[0] + number[1];
