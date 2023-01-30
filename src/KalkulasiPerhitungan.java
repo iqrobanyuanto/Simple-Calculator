b0b377177192ea4a313cd1b129487ac6e28ae6ed
Xeloreon <94229507+iqrobanyuanto@users.noreply.github.com>
2023-01-30 16:30:55 +0700

    update kalkulasi perhitungan

--- src/KalkulasiPerhitungan.java
+++ src/KalkulasiPerhitungan.java
@@ -1,7 +1,6 @@
 public class KalkulasiPerhitungan {
     public static void defOP(Integer number[], char operator){
         int r = 0;
-        //
         switch(operator){
             case '+':
             r = number[0] + number[1];
